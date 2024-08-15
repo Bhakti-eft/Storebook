1fe2972e3380491d8d4b11989272669dd796d81e
Bhakti <b.priya0102@gmail.com>
2024-08-15 22:13:15 +0530

    Backend Done !

--- Frontend/src/home/Home.jsx
+++ Frontend/src/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
+import Freebook from "../components/Freebook";
 import Footer from "../components/Footer";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <>
       <Navbar />
       <Banner />
+      <Freebook />
       <Footer />
     </>
   );
